931ab39a9e5b6d792e070d6b23cc02d9fe842efd
witrdotnet <witr.net@gmail.com>
2018-03-30 07:40:20 +0200

    move out app.component html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hb-root',
-  template: `
-  <mat-sidenav-container class="mainContainer">
-    <mat-sidenav style="background-color:#333" #sidenav mode="side" [(opened)]="opened">
-      <hb-sidebar></hb-sidebar>
-    </mat-sidenav>
-    <mat-sidenav-content>
-      <div style="text-align:center">
-        <hb-menu [sidenav]="sidenav"></hb-menu>
-        <h1>Welcome to {{title | hbtranslate}}!</h1>
-      </div>
-      <router-outlet></router-outlet>
-    </mat-sidenav-content>
-  </mat-sidenav-container>
-  `,
-  styles: [`
-  .mainContainer{
-  	position: absolute;
-  	top: 0;
-  	bottom: 0;
-  	left: 0;
-  	right: 0;
-  }
-`]
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'hb';
