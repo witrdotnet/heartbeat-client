8adeb2653e9c75998d69b146d04b8ecc8ba4ce62
witrdotnet <witr.net@gmail.com>
2019-02-21 00:50:02 +0100

    welcome on port 80 (for letsencrypt certbot)

--- docker/startup-docker.sh
+++ docker/startup-docker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
 docker run \
  --name heartbeat-client \
- -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf \
- -v $(pwd)/nginx-server.crt:/etc/nginx/ssl/server.crt \
- -v $(pwd)/nginx-server.key:/etc/nginx/ssl/server.key \
- -v $(pwd)/logs/:/var/log/nginx/ \
- -v $(pwd)/../dist:/var/www/heartbeat \
+ -v ${SCRIPT_DIR}/nginx.conf:/etc/nginx/conf.d/default.conf \
+ -v ${SCRIPT_DIR}/nginx-server.crt:/etc/nginx/ssl/server.crt \
+ -v ${SCRIPT_DIR}/nginx-server.key:/etc/nginx/ssl/server.key \
+ -v ${SCRIPT_DIR}/logs/:/var/log/nginx/ \
+ -v ${SCRIPT_DIR}/welcome:/var/www/welcome \
+ -v ${SCRIPT_DIR}/../dist:/var/www/heartbeat \
  -p 443:443 \
+ -p 80:80 \
  -d nginx
